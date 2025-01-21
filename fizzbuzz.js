292362e8b705efc9986a464c73185f97113a6fad
Jamilya Aitakunova <jamilya.aitakunova@softwire.com>
2025-01-21 11:10:24 +0000

    Printed Fizz, Buzz and FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,18 +6,16 @@ function fizzbuzz() {
     console.log('Welcome to FizzBuzz!');
     console.log('Hello World!');
     for(let i=0; i<101; i++){
-        if (i % 15 == 0)
+        if (i % 15 == 0) // if the number is a multiple of both 3 and 5 print FizzBuzz
             console.log("FizzBuzz");
-        else if (i % 5 == 0)
+        else if (i % 5 == 0) // if the number is a multiple of 5 print Buzz
             console.log("Buzz");
-        else if (i % 3 == 0)
+        else if (i % 3 == 0) // if the number is a multiple of 3 print Fizz
             console.log("Fizz");
         else console.log(i);
     }
 
 
-
-    // Put your code here...
 
     
 }
