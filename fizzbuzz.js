c9da33ac95d07e5ab7a926b4c48130ed70c63d7a
Jamilya Aitakunova <jamilya.aitakunova@softwire.com>
2025-01-21 10:21:15 +0000

    printed from 1 to 100

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,7 +5,7 @@ function fizzbuzz() {
     
     console.log('Welcome to FizzBuzz!');
     console.log('Hello World!');
-    for(let i=0; i<100; i++){
+    for(let i=0; i<101; i++){
         console.log(i)
     }
 
